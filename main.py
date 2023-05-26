f4a495d7bfb301956982791857b707ab68cea086
syedm1 <31075630+syedm1@users.noreply.github.com>
2023-05-26 19:44:45 +1000

    update exceptions

--- main.py
+++ main.py
@@ -2,15 +2,52 @@ from flask import Flask, jsonify, render_template
 import os
 import random
 import json
+import logging
+
+# Configure the logging module
+logging.basicConfig(level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
 
 app = Flask(__name__)
 
 quotes = []
 
+fall_back_quote = {
+    "Quote": "Don't cry because it's over, smile because it happened.",
+    "Author": "Dr. Seuss",
+    "Tags": [
+        "attributed-no-source",
+        "cry",
+        "crying",
+        "experience",
+        "happiness",
+        "joy",
+        "life",
+        "misattributed-dr-seuss",
+        "optimism",
+        "sadness",
+        "smile",
+        "smiling "
+    ],
+    "Popularity": 0.15566615566615566,
+    "Category": "life"
+}
+
 
 def load_quotes():
     with open('quotes.json', 'r', encoding='utf-8') as file:
         quotes.extend(json.load(file))
+
+
+def get_quote():
+    try:
+        quote = random.choice(quotes)
+        return jsonify({'quote': quote})
+    except Exception as e:
+        logging.exception(e)
+        return jsonify({'quote': fall_back_quote})
 
 
 @app.route('/diagnostic', methods=['GET'])
@@ -19,9 +56,11 @@ def diagnostic():
 
 
 @app.route('/api/quote', methods=['GET'])
-def get_quote():
-    quote = random.choice(quotes)
-    return jsonify({'quote': quote})
+def send_quote():
+    try:
+        return get_quote()
+    except:
+        return jsonify({'quote': 'No quote found'}), 500
 
 
 @app.route('/')
